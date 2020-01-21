220b7b84e093064c1222932d9e91ea4abcf5ad16
ultramanbabe <ultraman_babeclub@hotmail.com>
2020-01-21 15:36:11 +0700

    Done

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -12,20 +12,20 @@ int main(){
     if(age <= 20){
         cout << "Input height: ";
         cin >> height;
-        if(height <= 160){
+        if(height < 160){
             status = "UNFRIEND";
-        }else if(height <= 175 && height > 160){
+        }else if(height < 175 && height >= 160){
             status = "FRIEND";
         }else{
             cout << "Input money: ";
             cin >> money;
-            if(money < 69000000){
+            if(money <= 69000000){
                 status = "ONE-NIGHT-STAND";
             }else{
                 status = "MARRIED";
             }
         }
-    }else if(age <= 30 && age > 20){
+    }else if(age < 30 && age > 20){
         cout << "Input money: ";
         cin >> money;
         if(money > 10000000){
